12f00345a07acea3c41a0eb97b9837ab8d87f729
westpol <alexander.hitzinger@gmail.com>
2018-02-21 19:44:40 +0100

    Added Dir/File Count

--- Program.cs
+++ Program.cs
@@ -6,16 +6,9 @@ using System.Linq;
 namespace LineCounter {
     class Program {
 
-        public static Program instance;
-
         static void Main(string[] args) {
             Program p = new Program();
-            p.Setup();
             p.Main();
-        }
-
-        public void Setup() {
-            instance = this;
         }
 
         public void Main() {
@@ -31,24 +24,43 @@ namespace LineCounter {
             Console.WriteLine("Counting Lines...");
             List<string> paths = getFilePaths(dir, fileEnding);
             List<int> counts = new List<int>();
+            List<string> dirs = getDirPaths(dir);
+            int fileCount, dirCount;
+            fileCount = 0;
+            // Add 1 to include current Directory
+            dirCount = dirs.Count + 1;
             foreach (string dirPath in paths) {
                 int tmpCount = 0;
                 if (File.Exists(dirPath) && dirPath != Path.Combine(Directory.GetCurrentDirectory(), AppDomain.CurrentDomain.FriendlyName)) {
                     Console.WriteLine("> File found: " + dirPath);
                     tmpCount = File.ReadAllLines(dirPath).Length;
                     Console.WriteLine(tmpCount.ToString() + " Lines");
+                    fileCount++;
                 }
                 counts.Add(tmpCount);
             }
             Console.WriteLine("-------------------------");
             Console.WriteLine("Line count in all files: ");
             Console.WriteLine(counts.Sum().ToString("N0"));
+            Console.WriteLine("Files:");
+            Console.WriteLine(fileCount.ToString("N0"));
+            Console.WriteLine("Searched Directories:");
+            Console.WriteLine(dirCount.ToString("N0"));
             Console.WriteLine("Press any key to restart");
             Console.WriteLine("-------------------------");
             Console.WriteLine("ostpol.it | 2018");
             Console.ReadKey();
             Console.Clear();
             Main();
+        }
+
+        public List<string> getDirPaths(string dir) {
+            if (Directory.Exists(dir)) {
+                return Directory.GetDirectories(dir, "*", SearchOption.AllDirectories).ToList();
+            } else {
+                Console.WriteLine("Directory not found! \"" + dir + "\"");
+                return null;
+            }
         }
 
         public List<string> getFilePaths(string dir, string ending) {
